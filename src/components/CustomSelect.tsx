2dbcc431282f337ee67e2f3970eb284b68604ecb
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-01-18 22:16:21 +0300

    refactor: add some styles

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { ElementsList } from "./ElementsList";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import "./CustomSelect.css";
 
 export const CustomSelect = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleSelectClick = () => setIsOpen(!isOpen);
   return (
     <>
-      <button onClick={handleSelectClick}>CustomSelect</button>
+      <h1>My Custom Select</h1>
+      <button className={"btn"} onClick={handleSelectClick}>
+        CustomSelect
+        {!isOpen ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+      </button>
       {isOpen && <ElementsList />}
     </>
   );
--- src/components/ElementsList.tsx
+++ src/components/ElementsList.tsx
@@ -1,6 +1,6 @@
 export const ElementsList = () => {
   return (
-    <ul>
+    <ul className="list">
       <li>first element</li>
       <li>second element</li>
       <li>third element</li>
